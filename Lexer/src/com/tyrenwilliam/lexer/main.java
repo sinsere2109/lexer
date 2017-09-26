7ec1950969156f78b2d0238c2834cbc028440bcd
sinsere2109 <tyrenvillanueva@gmail.com>
2017-09-25 18:57:57 -0700

    fixed token

--- Lexer/src/com/tyrenwilliam/lexer/main.java
+++ Lexer/src/com/tyrenwilliam/lexer/main.java
@@ -5,7 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String s="hello hey";
+		String s="12 14 15";
 		Lexer lex = new Lexer(s);
 		
 	}
